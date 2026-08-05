8c5f335ffe6ca502d3e8a5b3d2587cb119760965
jabin <khadka.jabin10@gmail.com>
2026-08-05 19:07:22 +0545

    move order page and tracking page to react

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrderPage } from "./pages/orderPage";
+import { TrackingPage } from "./pages/TrackingPage,";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrderPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
     </Routes>
   );
 }
