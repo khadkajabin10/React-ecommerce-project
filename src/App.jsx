31de456a86cb9d3210dacd49ed240480e504276b
jabin <khadka.jabin10@gmail.com>
2026-08-22 17:28:06 +0545

    Create add to cart Feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,20 @@ import { TrackingPage } from "./pages/tracking/TrackingPage,";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    //console.log(response.data); //; you can see product add to cart
+    setCart(response.data);
+  };
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get("/api/cart-items?expand=product");
-      //console.log(response.data); //; you can see product add to cart
-      setCart(response.data);
-    };
-    fetchAppData();
+    loadCart();
   }, []);
   return (
     <Routes>
-      <Route path="/" element={<HomePage cart={cart} />}></Route>
+      <Route
+        path="/"
+        element={<HomePage cart={cart} loadCart={loadCart} />}
+      ></Route>
       <Route path="checkout" element={<CheckoutPage cart={cart} />}></Route>
       <Route path="orders" element={<OrderPage cart={cart} />}></Route>
       <Route path="tracking" element={<TrackingPage />}></Route>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,7 +3,7 @@ import "./HomePage.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { ProductsGrid } from "./ProductsGrid";
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -19,7 +19,7 @@ export function HomePage({ cart }) {
       <title>Ecommerce Project</title>
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
