92999d4289d808054c6aa77e620ec4b327a430a6
jabin <khadka.jabin10@gmail.com>
2026-08-30 12:53:11 +0545

    test clicking add to cart button

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,6 +1,9 @@
 import { it, expect, describe, vi } from "vitest";
 import { Product } from "./Product";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
+import axios from "axios"; //bex of vimock this is fake version of axios
+vi.mock("axios");
 
 describe("Product Component", () => {
   it("Displays the product details correctly", () => {
@@ -31,5 +34,29 @@ describe("Product Component", () => {
       "images/ratings/rating-45.png",
     );
     expect(screen.getByText("87")).toBeInTheDocument();
+  });
+  it("adds a produt to the cart", async () => {
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+    const loadCart = vi.fn(); //fake bec to records "i was called or not"
+
+    render(<Product product={product} loadCart={loadCart} />);
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToCartButton);
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    }); //here i try to make same funciton as add to cart but use fake so to check record wherther it is same same called or not
+    expect(loadCart).toHaveBeenCalled();
   });
 });
